b95a3630b5b96ba633d9e0edaaee838d1097f6ea
Priya <priya049btece23@igdtuw.ac.in>
2026-03-25 15:58:11 +0000

    Express Configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.get("/", (req,res) => {
     res.send("Welcome to udemy");
 });
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+app.use(cors({
+    origion: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Authorization", "content-Type"],
+}),
+);
 
 export default app;
